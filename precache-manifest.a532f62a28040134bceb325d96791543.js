eb6a0470d01bc64f37c7c3e9a120eb0bd7db1442
EfraimKrug <efraimmkrug@gmail.com>
2020-08-10 22:52:34 -0400

    Updates

---
+++ precache-manifest.a532f62a28040134bceb325d96791543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcda8bf9b2db23eaeeda8a7d51d1fe28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9018b8c35c6ff4e70113",
+    "url": "/static/css/main.635c886e.chunk.css"
+  },
+  {
+    "revision": "890751f04452cde81ee6",
+    "url": "/static/js/2.04402fbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04402fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9018b8c35c6ff4e70113",
+    "url": "/static/js/main.46c13962.chunk.js"
+  },
+  {
+    "revision": "4e9c07fb389e335d3c25",
+    "url": "/static/js/runtime-main.47306e53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
